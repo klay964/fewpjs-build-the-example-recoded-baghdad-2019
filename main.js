c6f5b684f3afc5de3fd1fd695a69c8631628ee47
Mohammed Ali <dody200039@gmail.com>
2019-12-11 21:27:11 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -33,20 +33,3 @@ for (let glyph of articleHearts) {
   glyph.addEventListener("click", likeCallback);
 }
 
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
